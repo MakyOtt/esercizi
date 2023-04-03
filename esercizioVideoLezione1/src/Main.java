d1b9c6af4e68c9438ec72be01a8e519ba0cdf759
MakyOtt <marika.ottana20@gmail.com>
2023-04-03 17:01:41 +0200

    esercizio video lezione 1

---
+++ esercizioVideoLezione1/src/Main.java
@@ -0,0 +1,36 @@
+
+public class Main {
+    public static void main(String[] args) {
+        exerciseOne();
+        exerciseTwo();
+        exerciseThree();
+    }
+
+    /**
+     VIDEO
+     * 1: Change (my name) to your name*/
+     public static void exerciseOne() {
+         System.out.println("Hello Marika!"); //<--- Edit this line
+     }
+     /**
+
+     * 2: Add a new println statement underneath this and put your name in it, *this result should look exactly the same as in exercise one if done
+     * correctly!
+     */
+    public static void exerciseTwo() {
+        System.out.print("Hello");
+        System.out.println(" Marika!");
+
+    }
+    /**
+
+     //Write code underneath this
+     //Write code above this
+     * 3: Edit this code so when it 'prints' to the console, it all appears on the same line * Expected print = 'This should all appear on the same line'
+     */
+    public static void exerciseThree() {
+        System.out.print("This should ");
+        System.out.print("all appear ");
+        System.out.print("on the same line ");
+    }
+}
